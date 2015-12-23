d6bb7c368ac2786fd2ac18c4951201991501c969
Hari Mahadevan <hari@smallpearl.com>
2015-12-23 23:50:38 +0800

    Fix broken/incomplete setup script and move it to root folder

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="singleurlcrud",
+    description="Django CRUD using a single view, and hence single URL, for all CRUD operations",
+    long_description=open('README.md').read(),
+    version="0.2",
+    author="Hari Mahadevan",
+    author_email="hari@hari.xyz",
+    url="http://www.github.com/harikvpy/singleurlcrud",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['django-bootstrap3'],
+    license="BSD2",
+    keywords="django crud",
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: Approved :: BSD2 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        ],
+    )

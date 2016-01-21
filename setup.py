0d1de5607bea9b0602ef8d507bb61baba40d8427
Hari Mahadevan <hari@smallpearl.com>
2016-01-21 17:04:00 +0800

    Update setup.py for URL and misc items

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="singleurlcrud",
-    description="Django CRUD using a single view, and hence single URL, for all CRUD operations",
+    name='singleurlcrud',
+    description='Django CRUD, implemented using a single view and consequently a single URL.',
     long_description=open('README.md').read(),
-    version="0.13",
-    author="Hari Mahadevan",
-    author_email="hari@hari.xyz",
-    url="http://www.github.com/harikvpy/singleurlcrud",
+    version='0.13',
+    author='Hari Mahadevan',
+    author_email='hari@hari.xyz',
+    url='https://www.github.com/harikvpy/crud/tarball/0.13',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['django-pure-pagination', 'django-bootstrap3'],
-    license="BSD2",
-    keywords="django crud",
+    license='BSD-3',
+    keywords=['django', 'crud'],
     classifiers=[
         'Development Status :: 1 - Alpha',
         'Environment :: Web Environment',
